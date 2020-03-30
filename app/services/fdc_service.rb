b03409ed4e95ae95c8cc602204cf9f487e13de52
David Atkins <atki1080@gmail.com>
2020-03-30 10:01:16 -0600

    Add fdc service

---
+++ app/services/fdc_service.rb
@@ -0,0 +1,13 @@
+class FdcService
+  def get_food_data(search_params)
+    response = conn.get("/fdc/v1/search?ingredients=#{search_params}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+    def conn
+      Faraday.new(url: "https://api.nal.usda.gov") do |f|
+        f.params["api_key"] = ENV['FDC_API_KEY']
+      end
+    end
+end
