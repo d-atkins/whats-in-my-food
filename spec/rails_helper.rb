1b6f08d203a7c3eaa07c451be48e256d7c4c246b
David Atkins <atki1080@gmail.com>
2020-03-30 10:18:37 -0600

    Add webmock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,3 +55,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<FDC_API_KEY>') { ENV['FDC_API_KEY'] }
+  config.configure_rspec_metadata!
+end
