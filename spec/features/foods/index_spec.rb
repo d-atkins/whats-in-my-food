4476a0445883d02b54b4bbaaacf93ae26232a358
David Atkins <atki1080@gmail.com>
2020-03-30 10:29:37 -0600

    Add test for search results

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -10,8 +10,20 @@ describe 'From the welcome page:', type: :feature do
 
     it 'returns a list of foods containing that ingredient' do
       expect(current_path).to eq(foods_path)
-      
+      expect(page).to have_css('#search_result', count: 50)
+      within('#search_result', match: :first) do
+        expected = {
+          description: 'REAL VEGETABLE CHIPS',
+          code: '728229015529',
+          brand_owner: 'The Hain Celestial Group, Inc.',
+          ingredients: 'A BLEND OF SWEET POTATOES (SWEET POTATO, PURPLE SWEET POTATO, BATATA), AVOCADO OIL, SEA SALT.'
+        }
+
+        within('#description') { expect(page).to have_content(expected[:description]) }
+        within('#gtinupc_code') { expect(page).to have_content(expected[:code]) }
+        within('#brand_owner') { expect(page).to have_content(expected[:brand_owner]) }
+        within('#ingredients') { expect(page).to have_content(expected[:ingredients]) }
+      end
     end
   end
-
 end
