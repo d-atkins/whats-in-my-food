e132a3841b888ab7212bd20876e58fcebbfde2ca
David Atkins <atki1080@gmail.com>
2020-03-30 10:11:09 -0600

    Add attr_readers

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,4 +1,6 @@
 class Food
+  attr_reader :description, :code, :brand_owner, :ingredients
+
   def initialize(attributes)
     @description = attributes[:description]
     @code = attributes[:gtinUpc]
